6085ce8ba1ee00e4654bd7122153cafbf5519733
Akshaykumar B <154233186+AkshaykumarBijjaragi@users.noreply.github.com>
2024-11-28 23:39:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -24,29 +24,5 @@ function showSlide() {
 }
 setInterval(showSlide, 3000); // Change slide every 3 seconds
 
-const slides = document.querySelector('.slides');
-let index = 0;
-const yearSpan = document.getElementById('currentYear');
-yearSpan.textContent = new Date().getFullYear();
-document.getElementById('copyButton').addEventListener('click', function() {
-  const upiID = document.getElementById('upiID').innerText;
-  navigator.clipboard.writeText(upiID)
-    .then(() => {
-      const message = document.getElementById('copyMessage');
-      message.style.display = 'block';
-      setTimeout(() => {
-        message.style.display = 'none';
-      }, 2000);
-    })
-    .catch(err => {
-      alert('Failed to copy UPI ID. Please try manually.');
-      console.error('Copy failed', err);
-    });
-});
-function showSlide() {
-  index++;
-  if (index >= slides.children.length) index = 0;
-  slides.style.transform = `translateX(-${index * 100}%)`;
-}
-setInterval(showSlide, 3000); // Change slide every 3 seconds
+
 
